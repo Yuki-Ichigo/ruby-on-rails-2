684486776f8dfe4bca810b2ea799eb9daf0f1144
Yuki-Ichigo <bzpl1215@gmail.com>
2020-06-22 15:48:41 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+# 以下Refileエラーにて追記
+Refile.secret_key = '3011282c77997a9c86d4d562b0e3a853406a5fb4a9579e8b545a49a00baeaabaf4a89a00aa70f225dd1ffc64a18e25c63bc0c25040cf59c2145b243ae7da71bc'
